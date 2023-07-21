ea9ad4b1994b7946761406368abecb72477cc009
Volodymyr <131542075+VovkaMazurok@users.noreply.github.com>
2023-07-21 20:00:11 +0300

    added clas UkrainianAlphabet

--- src/UkrainianAlphabet.java
+++ src/UkrainianAlphabet.java
@@ -1,2 +1,15 @@
-public class UkrainianAlphabet {
+import java.util.ArrayList;
+
+public final class UkrainianAlphabet {
+    public static void UkrainianAlphabet(){
+
+
+        String abc = "АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩьЮЯ.,””:-!? ";
+        char[] abcArray = abc.toCharArray();
+
+        ArrayList<Character> alphabetChar = new ArrayList<>();
+        for (int i = 0; i < abcArray.length; i++) {
+            alphabetChar.add(abcArray[i]);
+        }
+    }
 }
