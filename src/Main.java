55bf3830cc2daf02d0e474178b9eeacc801df7e8
Volodymyr <131542075+VovkaMazurok@users.noreply.github.com>
2023-07-22 08:42:34 +0300

    added line Main

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,7 @@ import java.util.Scanner;
 
 //"C:\Users\Admin\javarush\text.txt"
 public class Main {
-    public static void main(String[] args) {
-
-        UkrainianAlphabet.UkrainianAlphabet();
+    public static void main(String[] args) throws Exception {
 
         Scanner scanner = new Scanner(System.in);
 
@@ -19,21 +17,49 @@ public class Main {
         System.out.println("Please enter key (any number)");
         int key = scanner.nextInt();
 
+        ArrayList<Character> textChar = new ArrayList<>();
+
         try (FileReader fileReader = new FileReader(file);
              BufferedReader bufferedReader = new BufferedReader(fileReader)) {
-
-            ArrayList<Character> textChar = new ArrayList<>();
 
             while (bufferedReader.ready()) {
                 char ch = (char) fileReader.read();
                 textChar.add(ch);
             }
-            // Зроблено зчитування ключа( цифри ) і тексту з файлу і перетворення файлу на char
+            System.out.println(textChar);
+
         } catch (FileNotFoundException e) {
-            System.out.println("The file does not exist or is not available. Try another one " + e );
+            System.out.println("The file does not exist or is not available. Try another one " + e);
         } catch (IOException e) {
             System.out.println("Something went wrong. Try again " + e);
         }
+
+        // Зроблено зчитування ключа( цифри ) і тексту з файлу і перетворення файлу
+
+        encryption(textChar, key);
+        decryption(textChar, key);
     }
+
+    private static void encryption(ArrayList textList, int key) throws Exception {
+        UkrainianAlphabet.ukrainianAlphabet();
+
+
+        ArrayList<Character> characterArrayList = new ArrayList<>();
+        for (int j = 0; j < textList.size(); j++) {
+            for (int i = 0; i < UkrainianAlphabet.alphabet.size(); i++) {
+                if (textList.get(j).equals(UkrainianAlphabet.alphabet.get(i))) {
+                    characterArrayList.add((char) UkrainianAlphabet.alphabet.get((i + key) % 41));
+                } else
+                    continue;
+            }
+        }
+        System.out.println(characterArrayList.toString());
+
+    }
+
+    private static void decryption(ArrayList arrayList, int key) {
+
+    }
+// Шифрування / розшифрування
 }
 
